6b265a80c7602051e4a571d9b3f5a08c1f7d28fa
Keith <capkeith43@gmail.com>
2026-05-21 08:12:15 +0800

    Centers and Evacuee

--- evacfinder.sql
+++ evacfinder.sql
@@ -54,6 +54,11 @@ CREATE TABLE `centers` (
   `available_facilities` varchar(300) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+
+
+
+
+
 -- --------------------------------------------------------
 
 --
